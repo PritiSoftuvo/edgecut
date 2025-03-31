3ae88b1dec3350e3f2421bab46fbf5912dd836b6
NaveenSoftuvo <naveen.softuvo@bridge18.com>
2025-03-31 10:09:55 +0530

    My code

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,13 +9,7 @@ function About() {
 
           <div class="section3">
             <h2 class="AboutHeading">ABOUT US</h2>
-            <p class="AboutSection">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              <br />
-              Minus quidem maiores perspiciatis, illo maxime
-              <br />
-              voluptatem a itaque suscipit.
-            </p>
+
             <button class="AboutBtn">Read More</button>
           </div>
         </div>
